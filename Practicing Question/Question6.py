412ca0ad8d0e2d0a4ccef96083a00a4e2a3a2f22
burnt776 <burnt776@gmail.com>
2020-10-10 01:17:58 +0530

    fa

--- Practicing Question/Question6.py
+++ Practicing Question/Question6.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__':
     n = int(input())
+    # here we fetch the value and split it by space and append in integer_list by map
     integer_list = map(int, input().split())
+    # here we convert the integer_list into tuple 
     tup = tuple(integer_list)
     print(hash(tup))
