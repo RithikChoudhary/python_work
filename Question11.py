c7bd13b4265840ae7bd9243870b811d4eb8fcc6a
burnt776 <burnt776@gmail.com>
2020-10-13 17:12:16 +0530

    using string formatter

---
+++ Question11.py
@@ -0,0 +1,9 @@
+# You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+def print_full_name(a, b):
+    print(f"Hello {a} {b}! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
